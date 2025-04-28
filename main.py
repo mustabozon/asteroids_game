464b36718ae1664ef334318055ed4416913ae60c
mustabozon <sphere.railcar@gmail.com>
2025-04-28 15:07:01 +0400

    Added circles class file

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    pygame.time.clock.tick()
+    dt = 0
 
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+            
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
