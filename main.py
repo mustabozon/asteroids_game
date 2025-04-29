0978083cc46b2e568d63837328a5294db3981f58
mustabozon <sphere.railcar@gmail.com>
2025-04-29 15:34:04 +0400

    Added the ability for the player to move left and right

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
+        
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
