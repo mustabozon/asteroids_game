3bc3f07e3412a288f21096f4d9b964f7b6470ba6
mustabozon <sphere.railcar@gmail.com>
2025-04-29 16:54:24 +0400

    Created two groups in main

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pygame
 from constants import *
 from player import Player
 
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,10 +19,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
